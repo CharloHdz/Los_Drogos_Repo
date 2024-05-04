967d9394fe46a86e79abf8b573df54780f1d8cc0
Carlos Mario Hernández León <84859398+CharloHdz@users.noreply.github.com>
2024-05-04 09:25:48 -0600

    Hice cambios

---
+++ Assets/Roca.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roca : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Primer Frame");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int x = 0;
+
+        print("Frame NO." + x++);
+    }
+}
