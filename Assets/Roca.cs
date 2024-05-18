d149d8811c93481a46ef61e76001473c185ae57d
Carlos Mario Hernández León <84859398+CharloHdz@users.noreply.github.com>
2024-05-18 00:12:43 -0600

    Update

--- Assets/Roca.cs
+++ Assets/Roca.cs
@@ -1,20 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Roca : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //entera
+    public int monedas = 0;
+
+    //flotante
+    public float vida = 100.1f;
+    public float velocidad = 0.5f;
+
+    //booleana
+    public bool EnSuelo = true;
+
+    //string
+    public string nombre = "Roca";
+
+
+    //Rigidbody2D
+    public Rigidbody2D rb;
+
+    //Text
+    public TextMeshProUGUI Label;
+
     void Start()
     {
-        print("Primer Frame");
+        print("juego iniciado");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        int x = 0;
+        //movimiento del personaje
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * velocidad, rb.velocity.y);
+        }
 
-        print("Frame NO." + x++);
+        if(Input.GetAxis("Vertical") != 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Input.GetAxis("Vertical") * velocidad);
+        }   
+
+        Label.text = nombre;
     }
 }
